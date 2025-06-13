ec0452c1edd5b8d13d8880662036f36d8661ff57
ACT-C1-19 <ACT-C1-19@DESKTOP-I4SCAC5>
2025-06-13 12:31:57 +0530

    This is my Entity class program

---
+++ C2TC_CoreJavaProgramD/src/Entityclass/DemoStudent.java
@@ -0,0 +1,24 @@
+package Entityclass;
+
+public class DemoStudent {
+
+	public static void main(String[] args) {
+	Student st = new Student();
+	st.setAge(20);
+	st.setName("DD");
+	st.setId(908);
+	System.out.println(st);
+
+	
+	
+	Student dd = new Student();
+	dd.setAge(20);
+	dd.setName("DD");
+	dd.setId(908);
+	System.out.println(dd);
+
+	
+	}
+
+}
+
