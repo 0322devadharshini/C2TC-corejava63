1c6d5a6af35a5b69b7eef7fb0724d73640a8c9cf
ACT-C1-19 <ACT-C1-19@DESKTOP-I4SCAC5>
2025-06-13 10:25:27 +0530

    This is my Demo Char program

--- C2TC_CoreJavaProgramD/src/day1/Devajava.java
+++ C2TC_CoreJavaProgramD/src/day1/Devajava.java
@@ -1,15 +1,27 @@
 package day1;
-
+import java.util.Scanner;
 public class Devajava {
 
 	public static void main(String[] args) {
-		int a,b,c;
-		a=Integer.parseInt(args[0]);
-		b=Integer.parseInt(args[1]);
-		c=a+b;
-				
 		
-		System.out.println("Addition of a "+a+" and b "+b+" is"+c);
+		char ch = 'a';
+		System.out.println(ch);
+
+		
+		char ch1 = 65;
+		System.out.println(ch1);
+
+		
+		char var1 = '\u00A7';
+		System.out.println(var1);
+
+		
+		char var2 = '\u20AC';
+		System.out.println(var2);
+
+		
+		int a = 'A';
+		System.out.println(a);
 
 	}
 
